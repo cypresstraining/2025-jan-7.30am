4909400a574671eb87bd64c28e7b5d3d120d5556
cypresstraining <cypresstraining7@gmail.com>
2025-01-10 08:15:58 +0530

    changed the state of experimentalStudio

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    experimentalStudio:true,
+    experimentalStudio:false,
     //testIsolation:false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
