7d5bf34c9504c7f919c56857598dfc4937950e6a
cypresstraining <cypresstraining7@gmail.com>
2025-01-10 08:14:18 +0530

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    experimentalStudio:false,
+    experimentalStudio:true,
     //testIsolation:false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
