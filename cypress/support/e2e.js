ce726103f8dbad926d88329ca43632e63211a02f
cypresstraining <cypresstraining7@gmail.com>
2025-03-06 08:23:05 +0530

    classroom file

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,4 +15,7 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+import sqlServer from 'cypress-sql-server';
+sqlServer.loadDBCommands();
 //import 'cypress-iframe';
+//import sqlServer from 'cypress-sql-server'
