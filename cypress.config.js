9d93c201c605581f732a717c32a19e360315ddb1
cypresstraining <cypresstraining7@gmail.com>
2025-03-06 08:22:24 +0530

    commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("cypress");
+const sqlServer = require('cypress-sql-server');
 
 module.exports = defineConfig({
   projectId: "yo9zrb",//this will provide the flexibility to the cypress app to connect with cloud 
@@ -23,6 +24,29 @@ experimentalOriginDependencies:true,
     //includeShadowDom:true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      // Database configuration
+      config.db = {
+        userName: "db_ab6037_n4705_1_admin",
+        //db_ab6037_n4705_1_admin
+        //username to connect to the databse
+        password: "Welcome@0508",
+        //password to connect the DB
+        server: "sql1002.site4now.net",
+        //sql1002.site4now.net - server URL
+        options: {
+          database: "db_ab6037_n4705_1",
+          // this which database we are trying to connect 
+          encrypt: true,
+          rowCollectionOnRequestCompletion: true,
+        },
+      };
+
+      // Load SQL Server plugin
+      const tasks = sqlServer.loadDBPlugin(config.db);
+      on("task", tasks);
+
+
+      
     },
   },
 });
--- cypress/e2e/advancedUI/Weekend sessions/DBtests/query.cy.js
+++ cypress/e2e/advancedUI/Weekend sessions/DBtests/query.cy.js
@@ -1,18 +1,25 @@
-import * as allure from "allure-cypress";
-import { Severity } from "allure-js-commons";
+//import * as allure from "allure-cypress";
+//import { Severity } from "allure-js-commons";
+//import sqlServer from 'cypress-sql-server'
 
 describe('Database Query Tests', () => {
     it('execute a SQL query', () => {
-      allure.owner("Ram Prasad");
-      allure.tag("DB integration");
-      allure.tag("Regression test");
-      allure.severity(Severity.CRITICAL);
+  //    allure.owner("Ram Prasad");
+    //  allure.tag("DB integration");
+     // allure.tag("Regression test");
+      //allure.severity(Severity.CRITICAL);
       
-      //const query = 'SELECT * FROM your_table WHERE your_condition';
-      cy.sqlServer('select Name from [dbo].[Category]').then((result) => {
-        expect(result).to.not.be.empty; // Adjust assertions as needed
+      //const query =
+      //  'SELECT * FROM your_table WHERE your_condition';
+      cy.sqlServer('select Name from [dbo].[Category]')
+      .then((result) => {
+        expect(result).to.not.be.empty;
+         // Adjust assertions as needed
         // Perform more assertions on result
-        expect(result).to.be.lengthOf(34)
+        expect(result).to.be.lengthOf(3)//count of results 
+        // how many rows we do have 
+        // if you create 25 categories in your application then you need to suppy 25
+
         console.log(result[0][1])
         console.log(result[0][0])
         console.log(result[0][2])
@@ -39,17 +46,17 @@ describe('Database Query Tests', () => {
     });
 it('write a query to the customer table', function(){
 
-  allure.owner("Ram Prasad");
-  allure.tag("DB integration");
-  allure.tag("Regression test");
-  allure.severity(Severity.HIGH);
+  //allure.owner("Ram Prasad");
+  //allure.tag("DB integration");
+  //allure.tag("Regression test");
+  //allure.severity(Severity.HIGH);
 
 //how to send the query
 cy.sqlServer('select * from [dbo].[Customer]').then(function(result){
 console.log(result)
 //expect(result).to.not.be.empty; // Adjust assertions as needed
         // Perform more assertions on result
-        expect(result).to.be.lengthOf(788)
+        expect(result).to.be.lengthOf(7)
         console.log(result[0][1])
        
         cy.writeFile('cypress/fixtures/customerresults.json',result)
